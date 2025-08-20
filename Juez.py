f7c0728d236e7112838b36868a3ecaf9db53904f
Dcarrasc <David.carrascoo@usm.cl>
2025-08-20 13:35:46 -0400

    Update Juez.py
    Se comentan funciones de prueba y se arreglan detalles menores

--- Juez.py
+++ Juez.py
@@ -1,7 +1,7 @@
 import re
 import itertools
 
-#Estudio expreciones regulares
+#Estudio de expreciones regulares
 # '^' significa que busca desde el inicio de la cadena
 # '$' significa que busca hasta el final de la cadena
 # '[]' define un conjunto de caracteres permitidos
@@ -13,34 +13,19 @@ import itertools
 # '.' es un carácter comodín que representa cualquier carácter excepto una nueva línea
 # '\' es utilizado para escapar caracteres especiales
 # '-' permite el uso de guiones en palabras
-# 're.fullmatch' verifica si toda la cadena coincide con la expresión regular
-# 're.compile' compila la expresión regular para su uso posterior
-
 # 'r' indica que la cadena es una cadena cruda (raw string), lo que significa que los caracteres de escape se interpretan literalmente
 
-EBNF = re.compile(r"^[0-9AEIOUaeiouÁÉÍÓÚáéíóúA-Za-zÑñ¿?¡!,.;'-(): ]+$")
+# 're.fullmatch' verifica si toda la cadena coincide con la expresión regular
+# 're.compile' compila la expresión regular para su uso posterior
+# 're.search' busca el patrón en la cadena y devuelve un objeto de coincidencia si se encuentra
+# 're.findall' encuentra todas las coincidencias del patrón en la cadena y las devuelve como una lista
+
+
+
+EBNF = re.compile(r"^[0-9AEIOUaeiouÁÉÍÓÚáéíóúA-Za-zÑñ¿?¡!,.;'\-(): ]+$")
 letras = re.compile(r"[A-Za-zÑñÁÉÍÓÚáéíóú]+")
 vocales = re.compile(r"[AEIOUaeiouÁÉÍÓÚáéíóú]")
 
-
-#***
-# Parametro 1: Recibe como parametro el patrón a buscar
-# Parametro 2: Recibe como parametro el texto donde se buscará el patrón
-#***
-# Tipo de return: None
-#***
-# Descripción: Busca un patrón en un texto y muestra el resultado de la búsqueda. Si
-# el patrón se encuentra, muestra el texto encontrado y su posición; si no se encuentra, indica que no se encontró.
-def buscar_patron(patron, texto):
-    print(texto)
-
-    res = re.search(patron, texto)
-    if res:
-        print(f"Patrón encontrado: {res.group()} en la posición {res.start()} hasta : {res.end()}") 
-        print("----------------------------------------")
-    else:
-        print("Patrón no encontrado")
-        print("----------------------------------------")
 
 #*** 
 # Parametro 1: (String) Recibe como parametro el verso de una estrofa
@@ -56,9 +41,9 @@ def validacion_verso(verso):
 #***
 # Tipo de return: String
 #***
-# Descripción: Extrae la última palabra de un verso, ignorando signos de puntuación y espacios. Si no hay palabras válidas, retorna una cadena vacía.
+# Descripción: Extrae la última palabra de un verso, Decidí ignorar signos de puntuación y espacios para simplificar el calculo del bonus. Si no hay palabras válidas, retorna una cadena vacía.
 def ultimas_palabras(verso):
-    palabras = re.findall(r"[A-Za-zÑñÁÉÍÓÚáéíóú]+", verso.lower())
+    palabras = re.findall(r"[A-Za-zÑñÁÉÍÓÚáéíóú]+", verso)
     if palabras:
         return palabras[-1]
     else:
@@ -70,7 +55,7 @@ def ultimas_palabras(verso):
 #***
 # Tipo de return: Entero
 #***
-# Descripción: Compara las dos palabras y cuenta cuántas letras coinciden al final de ambas palabras. Si las palabras son iguales, retorna la longitud de la palabra.
+# Descripción: Funciínq que compara las dos palabras y cuenta cuántas letras coinciden al final de ambas palabras. Si las palabras son iguales, retorna la longitud de la palabra.
 def rima_consonante(palabra1, palabra2):
     palabra1 = palabra1.lower()
     palabra2 = palabra2.lower()
@@ -89,7 +74,7 @@ def rima_consonante(palabra1, palabra2):
 #***
 # Tipo de return: String
 #***
-# Descripción: Extrae las vocales de una palabra y las devuelve como una cadena. Convierte la palabra a minúsculas antes de extraer las vocales. Si no encuentra vocales, retorna una cadena vacía.
+# Descripción: Función con la que extraemos las vocales de una palabra y las devuelve como una cadena. Convierte la palabra a minúsculas antes de extraer las vocales. Si no encuentra vocales, retorna una cadena vacía.
 def extraer_vocales(palabra):
     return "".join(vocales.findall(palabra.lower()))
 
@@ -99,7 +84,7 @@ def extraer_vocales(palabra):
 #***
 # Tipo de return: Entero
 #***
-# Descripción: Compara las vocales de dos palabras y verifica si las vocales coinciden. Si coinciden, retorna el entero de cuantas coinciden; de lo contrario, deja de contar.
+# Descripción: Función que compara las vocales de dos palabras y verifica si las vocales coinciden. Si coinciden, retorna el entero de cuantas coinciden; de lo contrario, deja de contar.
 def rima_asonante(palabra1, palabra2):
     palabra1_vocales = extraer_vocales(palabra1)
     palabra2_vocales = extraer_vocales(palabra2)
@@ -112,7 +97,13 @@ def rima_asonante(palabra1, palabra2):
             break
     return contador
 
-
+#***
+# Parametro 1: (String) Recibe como parametro una palabra a evaluar
+# Parametro 2: (String) Recibe como parametro una palabra a evaluar
+#***
+# Tipo de return: Tupla (String, Entero)
+#***
+# Descripción: Función que determina el tipo de rima entre dos palabras y asigna el puntaje correspondiente. (notar que Hola != hola por lo que no la considera gemela debido al cambio de Mayusculas.)
 def puntaje_rima(palabra1, palabra2):
     if (palabra1 == palabra2):
         return 'gemela', 1  # Si son iguales, puntaje es la longitud de la palabra
@@ -120,7 +111,7 @@ def puntaje_rima(palabra1, palabra2):
     if puntaje_con >= 5:
         return 'consonante', 8
     elif puntaje_con >= 3:
-        return 'asonante', 5
+        return 'consonante', 5
     
     puntaje_ason = rima_asonante(palabra1, palabra2)
     if puntaje_ason >= 3:
@@ -135,12 +126,12 @@ def puntaje_rima(palabra1, palabra2):
 
     return 'sin rima', 0
 
-#Comenzamos el proceso del código
-with open("estrofas.txt", "r", encoding="utf-8") as f:
+
+with open("estrofas.txt", "r", encoding="utf-8") as f: #Comenzamos el proceso del código
     lineas = [line.strip() for line in f]
 
 
-palabras_bonus = lineas[0].split(",")  #Verificacion del bonus
+palabras_bonus = [p.strip() for p in lineas[0].split(",")]  # Verificación de palabras bonus eliminando espacios
 lineas = lineas[2:]
 
 estrofas = [] #Separar estrofas por líneas en blanco para separar los puntajes
@@ -156,53 +147,71 @@ if temp:
     estrofas.append(temp)
 
 
-with open("decision.txt", "w", encoding="utf-8") as out:
-    for i, estrofa in enumerate(estrofas, 1):
+with open("decision.txt", "w", encoding="utf-8") as out: #Una vez procesado las estrofas, comenzamos con el archivo de decisión.
+    for i, estrofa in enumerate(estrofas, 1): #Validación de 4 versos por estrofa
         if len(estrofa) != 4:
-            out.write(f"Estrofa {i}: Inválida (no tiene 4 versos)\n")
+            #print("logre entrar")
+            out.write(f"Estrofa {i}: Inválida\n")
             continue
 
-        if not all(validacion_verso(v) for v in estrofa):
+        if not all(validacion_verso(v) for v in estrofa): #Validación de versos dentro del EBNF, se imprime el detalle en caso de invalidación
+            for i, verso in enumerate(estrofa, 1):
+                if not validacion_verso(verso):
+                    print(f"Verso {i} inválido: {repr(verso)}")
+                    for caracter in verso:
+                        if not EBNF.fullmatch(caracter):
+                            print(f"  Carácter no permitido: {repr(caracter)} (Unicode: {ord(caracter)})")
             out.write(f"Estrofa {i}: Inválida (símbolos no permitidos)\n")
             continue
-
+        
         ultimas = [ultimas_palabras(v) for v in estrofa]
+        #print("Ultimas palabras de la estrofa:", ultimas)
 
         total = 0
         tipos_rimas_detectados = set()
-        rima_verso = [False] * 4
+        versos_con_rima = [False] * 4
         contador_sin_rima = 0
 
-        for a, b in itertools.combinations(range(4), 2):
+        for a, b in itertools.combinations(range(4), 2):  #Combinaciones de versos para evaluar rimas
             tipo, puntaje = puntaje_rima(ultimas[a], ultimas[b])
+            #print(tipo, puntaje)
             total += puntaje
 
+            if tipo == 'consonante':
+                tipos_rimas_detectados.add('consonante')
+                tipos_rimas_detectados.add('asonante')
+            else:
+                tipos_rimas_detectados.add(tipo)
+
             if tipo != 'sin rima':
-                rima_verso[a] = True
-                rima_verso[b] = True
+                versos_con_rima[a] = True
+                versos_con_rima[b] = True
             else:
                 contador_sin_rima += 1
-            tipos_rimas_detectados.add(tipo)
+            
         
-        #Penalización
-        if not all(rima_verso):
+        #Penalización (si ninguno es true, se penaliza ya que significa que no rima ninguno)
+        if not all(versos_con_rima):
             total -= 2
         
-        #Bonus
+        #Bonus (Se aplica solo una vez)
         bonus = False
         for palabra in ultimas:
             if palabra in palabras_bonus:
                 bonus = True
                 total += 2
                 break
-
+        
+        #Se formatea el puntaje final con la estructura que nos piden
         puntaje_final = round(total / 5, 1)
-        bonus_palabra = "(BONUS)" if bonus else ""
+        #print(total)
+        bonus_palabra = " (BONUS)" if bonus else ""
 
-
-        # antes de escribir:
-        orden = ['consonante', 'asonante', 'misma terminación', 'gemela']
+        
+        #Se formatea el tipo de rima detectado con la estructura que nos piden
+        orden = ['consonante', 'asonante', 'misma terminación', 'gemela'] 
+        #print(tipos_rimas_detectados)
         lista_tipos = [t for t in orden if t in tipos_rimas_detectados]
-        print(lista_tipos)
+        #print(lista_tipos)
         out.write(f"Estrofa {i}: {puntaje_final}/10{bonus_palabra}\n")
         out.write("Rimas: " + ", ".join(lista_tipos) + "\n")
